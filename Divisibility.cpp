855c45aba939ee8044f40b6efa674628d194338f
Ishan Gupta <ishangupta.work@gmail.com>
2016-02-10 13:29:31 +0530

    Code away, everyday..

--- Divisibility.cpp
+++ Divisibility.cpp
@@ -2,17 +2,16 @@
 #include <iostream>
 using namespace std;
 
-typedef long long ll;
+typedef long long int lli;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
-	ll a,b,k,count;
-	cin>>k>>a>>b;
-	ll left = a%k == 0 ? a : a + (k - a%k);
-   	ll right = b%k == 0 ? b : b - b%k;
-    count = (right - left)/k + 1;
-    cout<<count<<'\n';
+	lli e, s, d;
+	cin>>d>>s>>e;
+	e = e <  0 ? (e+1)/d - 1 : e/d;
+	s = s <= 0 ? s/d - 1 : (s-1)/d;
+	cout<<e-s<<'\n';
 	return 0;
 }
